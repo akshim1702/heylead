b119cffb7cc9a754d73aead5e0aba8035bfa1160
Akshay <akshayshrivastava1702@gmail.com>
2021-08-10 15:37:33 +0530

    initial

---
+++ js/section3_slick.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(function() {
+        $('.centerk').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            autoplay: false,
+            autoplaySpeed: 2000,
+            dots: false,
+            centerMode: true,
+            centerPadding: '0,'
+        })
+    })
+    });
+    
